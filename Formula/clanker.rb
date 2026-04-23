17843623bd3b2275ef74561333472524d87ffabd
bog <bnovykov@gmail.com>
2026-04-23 11:51:40 +0800

    release: 0.0.6

--- Formula/clanker.rb
+++ Formula/clanker.rb
@@ -2,15 +2,15 @@ class Clanker < Formula
   desc "AI-powered terminal for Cloud queries"
   homepage "https://github.com/bgdnvk/clanker"
 
-  version "0.0.2"
+  version "0.0.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bgdnvk/clanker/releases/download/v0.0.2/clanker_v0.0.2_darwin_arm64.tar.gz"
-      sha256 "da25a85c372f62657b037303ed76d17ee48d75e4a559802177512d33b277aab2"
+      url "https://github.com/bgdnvk/clanker/releases/download/v0.0.6/clanker_v0.0.6_darwin_arm64.tar.gz"
+      sha256 "7e9926ea68b68fb0a8b550e6165cdfffc37835c3f986b168f0b9f52ce2c0e82c"
     else
-      url "https://github.com/bgdnvk/clanker/releases/download/v0.0.2/clanker_v0.0.2_darwin_amd64.tar.gz"
-      sha256 "bcf2700119688223a485c1e294ae995b5f842b66af7916f7bd948b8db186df57"
+      url "https://github.com/bgdnvk/clanker/releases/download/v0.0.6/clanker_v0.0.6_darwin_amd64.tar.gz"
+      sha256 "afd2af7977dfb29adeaa40cc4a79add2794772ad455d6aca228d17205c639f3d"
     end
   end
 
